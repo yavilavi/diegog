ff89d2c6c5fed4c111d271b7f7ca9fdf62bd41dc
Yilmer Avila <yilmer0001@hotmail.com>
2020-07-04 19:56:39 -0500

    add aws db test 6

--- app/config.inc.php
+++ app/config.inc.php
@@ -6,11 +6,11 @@ $password='Galeano.1996';
 $nombre_base_datos='bditm';
 
 //rutas de la web
-define("SERVIDOR", "./index.php");
+define("SERVIDOR", "index.php");
 define("ADMINISTRADOR", "administracion.inc.php");
-define("RUTA_EDITAR_ENTRADA", "./validadormodalidad.php");
-define("RUTA_EDITAR_ARCHIVO", "./gestorpendientes.php");
-define("RUTA_ARCHIVO", "./archivos.php");
+define("RUTA_EDITAR_ENTRADA", "validadormodalidad.php");
+define("RUTA_EDITAR_ARCHIVO", "gestorpendientes.php");
+define("RUTA_ARCHIVO", "archivos.php");
 ?>
 
 
